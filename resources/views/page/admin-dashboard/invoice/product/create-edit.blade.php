737ae75d7a541f6873db7e91e894317be355eee7
Dipta Harimbawa <vipalkuda@gmail.com>
2024-10-20 00:47:49 +0700

    [fix] button submit create

--- resources/views/page/admin-dashboard/invoice/product/create-edit.blade.php
+++ resources/views/page/admin-dashboard/invoice/product/create-edit.blade.php
@@ -9,18 +9,20 @@
         <x-form.base url="{{ $data['url'] }}" method="POST">
             <x-form.select name="product_id" title="Product" :disable="is_null($notApproved) ? null : ($notApproved ? null : true)"
                 value="{{ $product->product_id ?? (request()->get('product_id') ?? '') }}" data="{!! $productResource !!}" />
-            <x-form.input :disable="is_null($notApproved) ? null : ($notApproved ? null : true)" name="quantity_received" label="Quantity" type="number" placeholder="input quantity"
-                value="{{ $product->quantity_received ?? '' }}" />
+            <x-form.input :disable="is_null($notApproved) ? null : ($notApproved ? null : true)" name="quantity_received" label="Quantity" type="number"
+                placeholder="input quantity" value="{{ $product->quantity_received ?? '' }}" />
             <x-form.input :disable="is_null($notApproved) ? null : ($notApproved ? null : true)" name="buy_price" label="Harga beli" type="number" placeholder="input buy price"
                 value="{{ $product->quantity_received ?? '' }}" />
             <x-form.input :disable="is_null($notApproved) ? null : ($notApproved ? null : true)" name="buy_notes" label="Catatan" placeholder="input buy notes"
                 value="{{ $product->quantity_received ?? '' }}" />
-            <x-form.input :disable="is_null($notApproved) ? null : ($notApproved ? null : true)" name="expired_at" label="Tanggal Expired" type="date" placeholder="input contact name"
-                value="{{ $product->expired_at ?? '' }}" />
-            @if (!$notApproved)
-            <x-button.submit  />
+            <x-form.input :disable="is_null($notApproved) ? null : ($notApproved ? null : true)" name="expired_at" label="Tanggal Expired" type="date"
+                placeholder="input contact name" value="{{ $product->expired_at ?? '' }}" />
+            @if (!Str::endsWith(request()->route()->getName(), '.edit'))
+                @if (!$notApproved)
+                    <x-button.submit />
+                @endif
             @endif
-            <x-button.cancel url="{{ $data['home'] }}" label="Kembali"/>
+            <x-button.cancel url="{{ $data['home'] }}" label="Kembali" />
         </x-form.base>
     </x-util.card>
 @endsection
